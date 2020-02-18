9de0a21ef020064f7780d0455cb6fde9f479612b
Jim <Jim@localhost>
2020-02-17 20:28:29 -0700

    good stopping point

--- JavaProjectsUdemy/src/studentDatabaseApp/Student.java
+++ JavaProjectsUdemy/src/studentDatabaseApp/Student.java
@@ -3,27 +3,54 @@
  */
 package studentDatabaseApp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 /**
  * @author Jim
  *
  */
 public class Student {
 	
+	public static enum Courses{
+		Hist101("History 101"),
+		Math101("Mathematics 101"),
+		Eng101("English 101"),
+		Chem101("Chemistry 101"),
+		CS101("Computer Science 101");
+		
+		private String longName;
+		
+		Courses(String longName){
+			this.longName = longName;
+		}
+		
+		public String getLongName() {
+			return this.longName;
+		}
+	}
+	private static float costPerClass = 600f;
+	private static int runningStudentID = 1000;
+	
+	
 	private String firstName;
 	private String lastName;
 	private int classYear = 1;
 	private int studentID;
 	private float accountBalance = 0.0f;
-	//TODO add list of classes enrolled
+	private List<Courses> enrolledCourses = new ArrayList<Courses>();
+	
 	
 	// Constructors
 	public Student(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		
-		//TODO input class year
+		this.inputClassYear();
 		
-		//TODO generate unique student ID
+		this.generateStudentID();
 	}
 	
 	
@@ -71,12 +98,6 @@ public class Student {
 		return studentID;
 	}
 	/**
-	 * @param studentID the studentID to set
-	 */
-	public void setStudentID(int studentID) {
-		this.studentID = studentID;
-	}
-	/**
 	 * @return the accountBalance
 	 */
 	public float getAccountBalance() {
@@ -96,5 +117,22 @@ public class Student {
 	public void addToBalance(float debt) {
 		this.accountBalance += debt;
 	}
+	private void inputClassYear() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter student class year:");
+		System.out.println("\t1 - Freshman");
+		System.out.println("\t1 - Sophomore");
+		System.out.println("\t1 - Junior");
+		System.out.println("\t1 - Senior");
+		this.classYear = in.nextInt();
+	}
+	private void generateStudentID() {
+		this.studentID = classYear * (int)1e5 + runningStudentID;
+		runningStudentID++;
+	}
 	
+	public void enrollCourses() {
+		//TODO generate enrollment code
+		
+	}
 }
